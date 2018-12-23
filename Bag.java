d84e56fc3ab90d696800b49fc1319ed7adfca420
Anusha Prasad <44678463+aprasad8008@users.noreply.github.com>
2018-12-23 22:53:13 +0530

    Add files via upload

---
+++ Bag.java
@@ -0,0 +1,73 @@
+/* 
+ * Bag.java
+ * 
+ * Author:          Computer Science S-111 staff
+ * Modified by:     Anusha Prasad, apsmileyface@gmail.com
+ * Date modified:   7/20/16
+ */
+
+/**
+ * An interface for a Bag ADT.
+ */
+public interface Bag {
+    /** 
+     * adds the specified item to the Bag.  Returns true on success
+     * and false if there is no more room in the Bag.
+     */
+    boolean add(Object item);
+
+    /** 
+     * removes one occurrence of the specified item (if any) from the
+     * Bag.  Returns true on success and false if the specified item
+     * (i.e., an object equal to item) is not in the Bag.
+     */
+    boolean remove(Object item);
+
+    /**
+     * returns true if the specified item is in the Bag, and false
+     * otherwise.
+     */
+    boolean contains(Object item);
+
+    /**
+     * returns true if the calling object contain all of the items in
+     * otherBag, and false otherwise.  Also returns false if otherBag 
+     * is null or empty. 
+     */
+    boolean containsAll(Bag otherBag);
+
+    /**
+     * returns the number of items in the Bag.
+     */
+    int numItems();
+
+    /**
+     * grab - returns a reference to a randomly chosen in the Bag.
+     */
+    Object grab();
+
+    /**
+     * toArray - return an array containing the current contents of the bag
+     */
+    Object[] toArray();
+    
+    //capacity- default max value returned
+    int capacity ();
+    
+    //isEmpty- returns false when there are no items in the bag, true otherwise
+    boolean isEmpty ();
+    
+    //numOccur- occurances of item in the bag 
+    int numOccur (Object item);
+    
+    //addItems- adds items to bag if there is space
+    boolean addItems (Bag other);
+    
+    //equals- need to have same number of occurances
+    boolean equals (Bag other);
+    
+    //unionWith- create and return an ArrayBag containing one occurrence of any item that is found in either the called object or the parameter other
+    Bag unionWith (Bag other);
+    
+    
+} 
